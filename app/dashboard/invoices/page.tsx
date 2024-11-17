6edc4c48b9be68453e9058e882447ed614b01def
Amandeep Singh <programmeramandeep@gmail.com>
2024-11-17 13:15:47 +0530

    feat: Add pagination functionality

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,10 +1,8 @@
+import { fetchInvoicesPages } from "@/app/lib/data";
 import { lusitana } from "@/app/ui/fonts";
 import { CreateInvoice } from "@/app/ui/invoices/buttons";
 import Pagination from "@/app/ui/invoices/pagination";
-import {
-  default as InvoicesTable,
-  default as Table,
-} from "@/app/ui/invoices/table";
+import { default as Table } from "@/app/ui/invoices/table";
 import Search from "@/app/ui/search";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import React, { Suspense } from "react";
@@ -18,6 +16,7 @@ const Page = async (props: {
   const searchParams = await props.searchParams;
   const query = searchParams?.query ?? "";
   const currentPage = Number(searchParams?.page ?? 1);
+  const totalPages = await fetchInvoicesPages(query);
 
   return (
     <div className="w-full">
@@ -32,7 +31,7 @@ const Page = async (props: {
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={} /> */}
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
